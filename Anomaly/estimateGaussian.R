1ba4c35599daf0ff3f21972cf2aece24ad086920
Dael <dael.separa@gmail.com>
2016-03-22 22:29:05 +0800

    Simplify implementation

--- Anomaly/estimateGaussian.R
+++ Anomaly/estimateGaussian.R
@@ -8,28 +8,15 @@ estimateGaussian <- function(X) {
 #   mu[n] mean of each feature
 #   var[n] variance of each feature (sigma^2)
 
-	# for repmat
-	require(pracma)
-	
 	m = nrow(X)
 	
-	# Compute the mean of each feature. This is for illustration purposes only. Best way to rewrite this is to use:
+	# Compute the mean of each feature
 	#
-	# mu = apply(X, 2, mean) ... instead
-	#
-	mu = apply(X, 2, sum)/m
+	mu = apply(X, 2, mean)
 	
 	# Compute standard deviation of each feature
 	#
-	# this can be rewritten as:
-	#
-	# sigma2 = apply(X, 2, var)*(m - 1)/m
-	#
-	# or:
-	#
-	# sigma2 = apply(X, 2, sd))^2*(m - 1)/m
-	#
-	sigma2 = apply((X - repmat(mu, m, 1))^2, 2, sum)/m
+	sigma2 = apply(X, 2, var)*(m - 1)/m
 	
 	return(list('mu' = mu, 'var' = sigma2));
 }

5bd0c3847cd6ce6c7ec7b53e4b5c46469c225ebd
Dael <dael.separa@gmail.com>
2016-03-23 02:28:46 +0800

    changed var->variance

--- Anomaly/multivariateGaussian.R
+++ Anomaly/multivariateGaussian.R
@@ -1,4 +1,4 @@
-multivariateGaussian <- function(X, mu = 0, var = 1) {
+multivariateGaussian <- function(X, mu = 0, variance = 1) {
 # Computes the multivariate gaussian distribution probability density function
 #
 # Inputs:
@@ -14,10 +14,10 @@ multivariateGaussian <- function(X, mu = 0, var = 1) {
   require(pracma)
   
   # create diagonal matrix whose primary diagonal elements contains the variance
-  if ((is.null(dim(var)) && length(var) > 0) || (nrow(var) == 1 || ncol(var) == 1 )) {
-    sigma2 = diag(var)
+  if ((is.null(dim(variance)) && length(variance) > 0) || (nrow(variance) == 1 || ncol(variance) == 1 )) {
+    sigma2 = diag(variance)
   } else {
-    sigma2 = var
+    sigma2 = variance
   }
   
   if (is.null(dim(mu))) {
@@ -32,7 +32,7 @@ multivariateGaussian <- function(X, mu = 0, var = 1) {
   Xn = X - repmat(mu, m, 1)
   
   # inv() may be used instead of solve()
-  p = (2*pi) ^ (-k/2) * det(sigma2) ^ (-0.5) * exp(-0.5*apply(Xn %* %solve(sigma2) * Xn, 1,sum))
+  p = (2*pi) ^ (-k/2) * det(sigma2) ^ (-0.5) * exp(-0.5*apply(Xn %*% solve(sigma2) * Xn, 1,sum))
   
   # reshape p into a column vector
   return(array(p, c(nrow(X), 1)))

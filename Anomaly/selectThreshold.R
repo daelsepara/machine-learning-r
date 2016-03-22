9bd95138c3bb34da7399f24bd960a53c7da71a60
Dael <dael.separa@gmail.com>
2016-03-23 03:16:38 +0800

    function to select the best prediction threshold (epsilon) to use. with NAN/Numeric checks on F1

---
+++ Anomaly/selectThreshold.R
@@ -0,0 +1,34 @@
+selectThreshold <- function(y, p) {
+# Determine best threshold to use to detect outliers
+#
+
+	epsilon_Best = 0
+	F1_Best = 0
+
+	stepsize = (max(p) - min(p)) / 1000
+	
+	for (epsilon in seq(min(p), max(p), by = stepsize)) {
+
+		predictions = p < epsilon
+
+		# determine true/false positives and negatives
+		tp = sum((predictions == TRUE) & (y == 1))
+		fp = sum((predictions == TRUE) & (y == 0))
+		fn = sum((predictions == FALSE) & (y == 1))
+
+		# compute precision and recall
+		prec = tp/(tp + fp)
+		rec = tp/(tp + fn)
+
+		# compute F1 score based on precision and recall
+		F1 =  2*prec*rec/(prec + rec)
+
+		if (!is.nan(F1) && is.numeric(F1) && F1 > F1_Best) {
+			F1_Best = F1
+			epsilon_Best = epsilon
+		}
+	}
+	
+	return(list('F1' = F1_Best, 'epsilon' = epsilon_Best))
+
+}

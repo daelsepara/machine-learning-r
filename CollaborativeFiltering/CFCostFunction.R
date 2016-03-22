72d4a086a14d39a6883b362b53b5eb0a1ae723f2
Dael <dael.separa@gmail.com>
2016-03-23 05:55:22 +0800

    Add function call to run optimizer

--- CollaborativeFiltering/CFCostFunction.R
+++ CollaborativeFiltering/CFCostFunction.R
@@ -44,3 +44,43 @@ CFOptimizeCost <- function(theta, P1, P2, P3 , P4, P5, P6) {
 
 	return(list('J' = result$J, 'grad' = grad))
 }
+
+CFOptimize <- function(X, Theta, Y, R, lambda, iterations = 100) {
+# Algorithm to use fmincg with CFCostFunction
+# Collaborative filtering cost function
+#
+# Inputs:
+#     X[m, n] product features (m - products, n - features)
+# Theta[m, n] model parameters, user features (m - users, n - features)
+#     Y[m, n] user ratings (m - products, n - users)
+#     R[m, n] = 1 if user rated product, 0 otherwise (m - products, n - users)
+#      lambda regularization parameter
+#  iterations maximum number of iterations to run optimizer
+#
+# Outputs:
+#    X, Theta  optimzed product and model features/parameters
+#    	 cost  final cost
+#  iterations  actual number of iterations used
+	
+	# determine data dimensions
+	num_users = ncol(X)
+	num_products = nrow(X)
+	num_features = ncol(Theta)
+
+	# initialize parameters with random values
+	randX = array(rnorm(n = num_products * num_users, mean = 0, sd = 1), c(num_products, num_users))
+	randTheta = array(rnorm(n = num_users * num_features, mean = 0, sd = 1), c(num_users, num_features))
+	
+	# unroll into a single vector
+	initial_params = c(as.vector(randX), as.vector(randTheta))
+	
+	# optimize cost function
+	result = fmincg(CFOptimizeCost, initial_params, iterations, Y, R, lambda, num_users, num_products, num_features)
+	
+	# roll up result into arrays
+	offs = num_products*num_features
+	optX = array(result$X[1:offs], c(num_products, num_features))
+	optTheta = array(result$X[(offs+1):length(result$X)], c(num_users, num_features))
+	
+	return(list('X' = optX, 'Theta' = optTheta, 'iterations' = result$iterations, 'cost' = result$cost))
+}

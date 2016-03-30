9fc6452a273724a31da900d3c28ee64e910125b0
Dael <dael.separa@gmail.com>
2016-03-31 00:22:27 +0800

    implement unpack() which unpacks a list into objects

--- SupportVectorMachine/smo.R
+++ SupportVectorMachine/smo.R
@@ -113,8 +113,8 @@ smo2 <- function(X, Y, krnel, kpar1, kpar2, C, tol, steps, eps, method) {
 	}
 
 	# unpack model into objects
-	for (i in 1:length(model)) assign(names(model)[i], model[[i]])
-				
+	unpack(model)
+			
 	if (method == 1 || method == 2) {
 		flag = (glob$b_up < glob$b_low - 2 * tol) || (stp >= steps)
 	} else {
@@ -125,10 +125,17 @@ smo2 <- function(X, Y, krnel, kpar1, kpar2, C, tol, steps, eps, method) {
 		cat('The algorithm has not converged. This may be due to:\n (a) the maximum number of iterations has been reached and convergence has not, yet, been achieved or \n (b) the chosen values for the hyperparameters (C as well as the parameters that define the kernel function) \n can not lead to a solution. \n')
 	}
 	
-	# unpack result into objects
-	for (i in 1:length(model)) assign(names(model)[i], model[[i]])
-	
 	return(list('alpha' = alpha, 'w' = w, 'b' = b, 'stp' = stp, 'evals' = evals, 'glob' = glob))
+}
+
+unpack <- function(X) {
+# unpacks a list X = list('variable1' = value1, 'variable2' = value2, ...)
+# into the objects variable1, variable2, ... whose values are value1, value2, ...
+	
+	# assign to variables in the parent/calling frame
+	for (i in 1:length(X)) {
+		assign(names(X)[i], X[[i]], envir = parent.frame())
+	}
 }
 
 SMO_Platt <- function(X, Y, krnel, kpar1, kpar2, C, tol, steps, eps) {
@@ -139,7 +146,7 @@ SMO_Platt <- function(X, Y, krnel, kpar1, kpar2, C, tol, steps, eps) {
 	
 	# initialize alpha array to all zero
 	alpha = array(0, c(n, 1))
-	w = array(0, c(1,D))
+	w = array(0, c(1, D))
 	b = 0
 	evals = 0
 
@@ -175,8 +182,9 @@ SMO_Platt <- function(X, Y, krnel, kpar1, kpar2, C, tol, steps, eps) {
 				}
 
 				result = examineExampleP(i, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, tol, steps, stp, evals, eps, K)
+
 				# unpack result into objects
-				for (i in 1:length(result)) assign(names(result)[i], result[[i]])
+				unpack(result)
 				
 				numChanged = numChanged + retval
 			}
@@ -202,7 +210,7 @@ SMO_Platt <- function(X, Y, krnel, kpar1, kpar2, C, tol, steps, eps) {
 				result = examineExampleP(glob$I_0[i], glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, tol, steps, stp, evals, eps, K)
 				
 				# unpack result into objects
-				for (i in 1:length(result)) assign(names(result)[i], result[[i]])
+				unpack(result)
 				
 				numChanged = numChanged + retval
 			}
@@ -259,7 +267,7 @@ examineExampleP <- function(i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C,
 			result = takeStepP(i1, i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, tol, evals, eps, K)
 			
 			# unpack result into objects
-			for (i in 1:length(result)) assign(names(result)[i], result[[i]])
+			unpack(result)
 			
 			if (retval == 1) {
 				return(list('retval' = retval, 'alpha' = alpha, 'w' = w, 'b' = b, 'stp' = stp, 'evals' = evals, 'glob' = glob))
@@ -278,7 +286,7 @@ examineExampleP <- function(i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C,
 			result = takeStepP(i1, i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, tol, evals, eps, K)
 			
 			# unpack result into objects
-			for (i in 1:length(result)) assign(names(result)[i], result[[i]])
+			unpack(result)
 			
 			if (retval == 1) {
 				return(list('retval' = retval, 'alpha' = alpha, 'w' = w, 'b' = b, 'stp' = stp, 'evals' = evals, 'glob' = glob))
@@ -297,7 +305,7 @@ examineExampleP <- function(i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C,
 			result = takeStepP(i1, i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, tol, evals, eps,K)
 			
 			# unpack result into objects
-			for (i in 1:length(result)) assign(names(result)[i], result[[i]])
+			unpack(result)
 			
 			if (retval == 1) {
 				return(list('retval' = retval, 'alpha' = alpha, 'w' = w, 'b' = b, 'stp' = stp, 'evals' = evals, 'glob' = glob))
@@ -508,9 +516,11 @@ SMO_Keerthi_modif1 <- function(X, Y, krnel, kpar1, kpar2, C, tol, steps, eps) {
 	examineAll = 1
 	
 	while (((numChanged > 0) || (examineAll == 1)) && (stp <= steps)) {
+		
 		numChanged = 0
 		
 		if (examineAll==1) {
+		
 			for (i in 1:n) {
 				
 				stp = stp + 1
@@ -522,11 +532,12 @@ SMO_Keerthi_modif1 <- function(X, Y, krnel, kpar1, kpar2, C, tol, steps, eps) {
 				result = examineExampleK(i, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, tol, steps, stp, evals, eps, K)
 				
 				# unpack result into objects
-				for (i in 1:length(result)) assign(names(result)[i], result[[i]])
+				unpack(result)
 				
 				numChanged = numChanged + retval
 			}
 		} else {
+		
 			k = length(glob$I_0)
 			
 			for (i in 1:k) {
@@ -542,25 +553,30 @@ SMO_Keerthi_modif1 <- function(X, Y, krnel, kpar1, kpar2, C, tol, steps, eps) {
 					break 
 				}
 				 
-				result = examineExampleK(glob$I_0[i], glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, tol, steps, stp, evals, eps,K)
+				result = examineExampleK(glob$I_0[i], glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, tol, steps, stp, evals, eps, K)
 				
 				# unpack result into objects
-				for (i in 1:length(result)) assign(names(result)[i], result[[i]])
-				
+				unpack(result)
+
 				numChanged = numChanged + retval
 				
 				# it is easy to check if optimality on I_0 is attained...
-				if ( (glob$b_up) > ( glob$b_low - (2*tol) ) ) {
+				if ( (glob$b_up) > (glob$b_low - (2*tol))) {
+				
 					# exit the loop after setting numChanged = 0
 					numChanged = 0
+					
 					break # [sdsepara] long comment (in Greek) removed here
 				}
 			}
 		}
 		
 		if (examineAll == 1) {
+			
 			examineAll = 0
+			
 		} else if (numChanged == 0) {
+			
 			examineAll = 1
 		}
 	}
@@ -594,7 +610,7 @@ SMO_Keerthi_modif2 <- function(X, Y, krnel, kpar1, kpar2, C, tol, steps, eps) {
 	# --------------------------------------------------------------------------
 
 	glob = list('fcache' = numeric(0), 'b_up' = numeric(0), 'b_low' = numeric(0), 'i_up' = numeric(0), 'i_low' = numeric(0), 'v_1' = numeric(0), 'v_2' = numeric(0), 'I_0' = numeric(0), 'I_1' = numeric(0), 'I_2' = numeric(0), 'I_3' = numeric(0), 'I_4' = numeric(0))
-	## initialize fcache array to all zero and its size to n
+	# initialize fcache array to all zero and its size to n
 	glob$fcache = array(0, c(n, 1))
 	# initialize b_up = -1, i_up to any one index of class 1
 	glob$b_up = -1
@@ -641,19 +657,19 @@ SMO_Keerthi_modif2 <- function(X, Y, krnel, kpar1, kpar2, C, tol, steps, eps) {
 				result = examineExampleK(i, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, tol, steps, stp, evals, eps,K)
 				
 				# unpack result into objects
-				for (i in 1:length(result)) assign(names(result)[i], result[[i]])
+				unpack(result)
 				
 				numChanged = numChanged + retval
 			}
 			
 		} else {
 			
-			#the following loop is the only difference between the two SMO
-			#modifications. Whereas, in modification 1, the inner loop selects
-			#i2 from I_0 sequentially, here i2 is always set to the current
-			#i_low and i1 is set to the current i_up clearly, this corresponds
-			#to choosing the worst violating pair using members of I_0 and some
-			#other indices.
+			# the following loop is the only difference between the two SMO
+			# modifications. Whereas, in modification 1, the inner loop selects
+			# i2 from I_0 sequentially, here i2 is always set to the current
+			# i_low and i1 is set to the current i_up clearly, this corresponds
+			# to choosing the worst violating pair using members of I_0 and some
+			# other indices.
 			
 			inner_loop_success = 1
 			
@@ -674,7 +690,7 @@ SMO_Keerthi_modif2 <- function(X, Y, krnel, kpar1, kpar2, C, tol, steps, eps) {
 				result = takeStepK(glob$i_up, glob$i_low, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, tol, evals, eps,K)
 				
 				# unpack result into objects
-				for (i in 1:length(result)) assign(names(result)[i], result[[i]])
+				unpack(result)
 				
 				numChanged = numChanged + retval
 			}
@@ -730,15 +746,13 @@ takeStepK <- function(i1, i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, t
 	}
 	
 	# --------------------------------------------------------------------------
-
-
-	## computation of the derivative eta
+	# computation of the derivative eta
 	# --------------------------------------------------------------------------
 	k11 = K[i1, i1]
 	k12 = K[i1, i2]
 	k22 = K[i2, i2]
 	evals = evals + 3
-	eta = (2*k12) - k11 - k22
+	eta = (2 * k12) - k11 - k22
 	
 	## computation of new alpha(i2)
 	if (eta<0) {
@@ -749,9 +763,9 @@ takeStepK <- function(i1, i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, t
 			a2 = H
 		}
 	} else {
-		##the derivative is 0 => we have to make optimization by other means
-		##Lobj = objective function at a2=L (according to Platt)
-		##Hobj = objective function at a2=H (according to Platt)
+		## the derivative is 0 => we have to make optimization by other means
+		## Lobj = objective function at a2=L (according to Platt)
+		## Hobj = objective function at a2=H (according to Platt)
 		L1 = alph1 + (s *(alph2 - L))
 		H1 = alph1 + (s *(alph2 - H))
 		f1 = (-y1 * F1) + (alph1 * k11) + (s * alph2 * k12)
@@ -768,40 +782,38 @@ takeStepK <- function(i1, i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, t
 		}
 	}
 	# --------------------------------------------------------------------------
-
-
 	# Calculate the change in a - if very small  return 0
-	## !!!!!NOTE!! if eps not small enought it may stop the algorithm early!!!!!!
+	# !!!!!NOTE!! if eps not small enought it may stop the algorithm early!!!!!!
 	# --------------------------------------------------------------------------
 	if (abs(a2 - alph2)< eps * (a2 + alph2 + eps)) {
 		retval = 0
 		return(list('retval' = retval, 'alpha' = alpha, 'w' = w, 'b' = b, 'evals' = evals, 'glob' = glob))
 	}
-	#--------------------------------------------------------------------------
+	# --------------------------------------------------------------------------
 
 
 	# computation on new a1pha1(a1)
-	#--------------------------------------------------------------------------
+	# --------------------------------------------------------------------------
 	a1 = alph1 + (s*(alph2-a2))
-	#--------------------------------------------------------------------------
+	# --------------------------------------------------------------------------
 
 
-	#Update weight vector to reflect change in a1 & a2, if linear SVM
-	#--------------------------------------------------------------------------
+	# Update weight vector to reflect change in a1 & a2, if linear SVM
+	# --------------------------------------------------------------------------
 	if (strcmpi(krnel, 'linear')) {
 		w = w + (y1 * (a1 - alph1) * X[i1, ]) + (y2 * (a2 - alph2) * X[i2, ])
 	}
-	#--------------------------------------------------------------------------
+	# --------------------------------------------------------------------------
 
 
 	# Store a1 and a2 in the alpha array
 	# --------------------------------------------------------------------------
 	alpha[i1] = a1
 	alpha[i2] = a2
-	#--------------------------------------------------------------------------
+	# --------------------------------------------------------------------------
 
-	#Update fcache[i] for i in I_0 using new Lagrange multipliers
-	#--------------------------------------------------------------------------
+	# Update fcache[i] for i in I_0 using new Lagrange multipliers
+	# --------------------------------------------------------------------------
 	k = length(glob$I_0)
 	
 	for (i in 1:k) {
@@ -817,7 +829,7 @@ takeStepK <- function(i1, i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, t
 	# about alpha_i being at 0, C or in between them. Using this together with
 	# target[i] gives information as to which index set i belongs.
 	# Update I_0, I_1, I_2, I_3, I_4
-	#--------------------------------------------------------------------------
+	# --------------------------------------------------------------------------
 	glob$I_0 = which(alpha > 0 && alpha < C)
 	glob$I_1 = which(alpha[glob$v_1] == 0)
 	glob$I_1 = glob$v_1[glob$I_1]
@@ -827,20 +839,20 @@ takeStepK <- function(i1, i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, t
 	glob$I_3 = glob$v_1[glob$I_3]
 	glob$I_4 = which(alpha[glob$v_2] == 0)
 	glob$I_4 = glob$v_2[glob$I_4]
-	#--------------------------------------------------------------------------
+	# --------------------------------------------------------------------------
 
 	# Compute updated F values for i1 and i2
-	#--------------------------------------------------------------------------
+	# --------------------------------------------------------------------------
 	glob$fcache[i1] = F1 + (y1 * (a1 - alph1) * k11) + (y2 * (a2 - alph2) * k12)
 	glob$fcache[i2] = F2 + (y1 * (a1 - alph1) * k12) + (y2 * (a2 - alph2) * k22)
 	# --------------------------------------------------------------------------
 
 
-	#Compute (i_low, b_low) and (i_up, b_up),
-	#using only i1, i2 and indices in I_0
+	# Compute (i_low, b_low) and (i_up, b_up),
+	# using only i1, i2 and indices in I_0
 
-	#--------------------------------------------------------------------------
-	#--GIA TO i1 -------------------------------------------------------
+	# --------------------------------------------------------------------------
+	# --GIA TO i1 -------------------------------------------------------
 	v = which(glob$I_1 == i1)
 	i1_in_I_1 = length(v)
 	v = which(glob$I_2 == i1)
@@ -850,7 +862,7 @@ takeStepK <- function(i1, i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, t
 	v = which(glob$I_4 == i1)
 	i1_in_I_4 = length(v)
 
-	## --------------------------------------------------------------------------
+	# --------------------------------------------------------------------------
 	# --GIA TO i2 -------------------------------------------------------
 	v = which(glob$I_1 == i2)
 	i2_in_I_1 = length(v)
@@ -955,12 +967,11 @@ examineExampleK <- function(i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C,
 		glob$b_low = F2
 		glob$i_low = i2
 	}
+
 	# ------------------------------------------------------------------
-
-
-	#Chech optimality using current b_low and b_up and, if
-	#violated, find an index i1 to do joint optimization with i2
-	#------------------------------------------------------------------
+	# Chech optimality using current b_low and b_up and, if
+	# violated, find an index i1 to do joint optimization with i2
+	# ------------------------------------------------------------------
 	optimality = 1
 	
 	if ((i2_in_I_0 + i2_in_I_1 + i2_in_I_2) > 0) {
@@ -999,10 +1010,10 @@ examineExampleK <- function(i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C,
 
 	stp = stp + 1
 
-	result =  takeStepK(i1, i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, tol, evals, eps, K)
+	result = takeStepK(i1, i2, glob, alpha, w, b, X, Y, krnel, kpar1, kpar2, C, tol, evals, eps, K)
 	
 	# unpack result into objects
-	for (i in 1:length(result)) assign(names(result)[i], result[[i]])
+	unpack(result)
 	
 	return(list('retval' = retval, 'alpha' = alpha, 'w' = w, 'b' = b, 'stp' = stp, 'evals' = evals, 'glob' = glob))
 }
@@ -1075,80 +1086,68 @@ CalcKernel <- function(u, v, ker, kpar1, kpar2) {
 	ker = tolower(ker)
 	
 	k = array(0, c(r1, 1))
-	
 	v = as.vector(v)
 	
 	if (strcmp(ker, 'linear')) {
 		k = u %*% v
 	} else if (strcmp(ker, 'poly')) {
-		k = (u %*% v + kpar1)^kpar2
+		k = (u %*% v + kpar1) ^ kpar2
 	} else if (strcmp(ker, 'rbf')) {
-
 	    for (i in 1:r1) {
-  			k[i] = exp(-(u[i,] - v) %*% t(u[i, ]-v)/(2*kpar1^2))
+			d = array(u[i, ] - v, c(1, length(v)))
+  			k[i] = exp(-d %*% t(d)/(2*kpar1^2))
 	    }
-	  
 	} else if (strcmp(ker, 'erbf')) {
-		
 		for (i in 1:r1) {
-			k[i] = exp(-sqrt((u[i, ] - v) %*% t(u[i, ]-v))/(2*kpar1^2))
+			d = array(u[i, ] - v, c(1, length(v)))
+			k[i] = exp(-sqrt(d %*% t(d))/(2 * kpar1 ^ 2))
 		}
 	} else if (strcmp(ker, 'sigmoid')) {
-
 		for (i in 1:r1) {
-			k[i] = tanh(kpar1*u[i, ] %*% v/length(u[i, ]) + kpar2)
+			k[i] = tanh(kpar1*u[i, ] %*% v/ length(u[i, ]) + kpar2)
 		}
 	} else if (strcmp(ker, 'fourier')) {
-		
 		for (i in 1:r1) {
-			z = sin(kpar1 + 1 / 2)* 2 * array(1, c(length(u[i, ]),1))
-			j = which(u[i,] - v != 0.0)
-			z[j] = sin(kpar1 + 1/2) * (u[i, j] - v[j])/sin((u[i, j] - v[j]) / 2)
+			z = sin(kpar1 + 1 / 2) * 2 * array(1, c(length(u[i, ]),1))
+			j = which(u[i, ] - v != 0.0)
+			z[j] = sin(kpar1 + 1 / 2) * (u[i, j] - v[j])/sin((u[i, j] - v[j]) / 2)
 			k[i] = prod(z)
 		}
 	} else if (strcmp(ker, 'spline')) {
-	
 		for (i in 1:r1) {
 			z = 1 + u[i, ] * v + u[i, ] * v * bsxfun('min', u[i, ], v) - ((u[i, ] + v) / 2) * (bsxfun('min', u[i, ], v)) ^ 2 + (1 / 3) * (bsxfun('min', u[i, ], v)) ^ 3
 			k[i] = prod(z)
 		}
 	}  else if (strcmp(ker, 'curvspline') || strcmp(ker, 'anova')) {
-			
 		for (i in 1:r1) {
 			z = 1 + u[i, ] * v + (1 / 2) * u[i, ] *v * bsxfun('min', u[i, ], v) - (1 / 6) * bsxfun('min', u[i, ], v) ^ 3
 			k[i] = prod(z)
 		}
 		
 	} else if (strcmp(ker, 'bspline')) {
-		
 		for (i in 1:r1) {
 			z = 0
-			for (r in 0:(2*(kpar1+1))) {
-				z = z + (-1) ^ r * choose(2*(kpar1+1), r) * (bsxfun('max', v, u[i, ] - v + kpar1 + 1 - r)) ^ (2 * kpar1 + 1)
+			for (r in 0:(2 * (kpar1 + 1))) {
+				z = z + (-1) ^ r * choose(2 * (kpar1 + 1), r) * (bsxfun('max', v, u[i, ] - v + kpar1 + 1 - r)) ^ (2 * kpar1 + 1)
 			}
-			
 			k[i] = prod(z)
 		}
 	} else if (strcmp(ker, 'anovaspline1')) {
-		
 		for (i in 1:r1) {
 			z = 1 + u[i, ] * v + u[i, ] * v * bsxfun('min', u[i, ], v) - ((u[i, ] + v) / 2) * bsxfun('min', u[i, ], v) ^ 2 + (1 / 3) * bsxfun('min', u[i, ], v) ^ 3
 			k[i] = prod(z)
 		}
 	} else if (strcmp(ker, 'anovaspline2')) {
-		
 		for (i in 1:r1) {
 			z = 1 + u[i, ] * v + (u[i, ] * v)^2 + (u[i, ] * v) ^ 2 * bsxfun('min', u[i, ], v) - u[i, ] * v * (u[i, ]+ v) * bsxfun('min', u[i, ], v) ^ 2 + (1 / 3) * (u[i, ] ^ 2 + 4 * u[i, ] * v + v ^ 2) * bsxfun('min', u[i, ], v) ^3 - (1 / 2) * (u[i, ] + v) * bsxfun('min', u[i, ], v) ^ 4 + (1 / 5) * bsxfun('min', u[i, ], v) ^ 5
 			k[i] = prod(z)
 		}
 	} else if (strcmp(ker, 'anovaspline3')) {
-		
 		for (i in 1:r1) {
 			z = 1 + u[i, ] * v + (u[i, ] * v) ^ 2 + (u[i, ] * v) ^ 3 + (u[i, ] * v) ^ 3 * bsxfun('min', u[i, ], v) - (3 / 2) * (u[i, ] * v) ^ 2 *(u[i, ] + v) * bsxfun('min', u[i, ], v) ^ 2 + u[i, ] * v * (u[i, ] ^ 2 + 3 * u[i, ] * v + v ^2) * bsxfun('min', u[i, ], v) ^ 3 - (1 / 4)*(u[i, ] ^ 3 + 9 * u[i, ] ^ 2  * v + 9 * u[i, ] * v ^ 2 + v ^ 3) * bsxfun('min', u[i, ], v) ^ 4 + (3 / 5) * (u[i, ] ^ 2 + 3 * u[i, ] * v + v ^ 2) * bsxfun('min', u[i, ], v) ^ 5 - (1/2) * (u[i, ] + v) * bsxfun('min', u[i, ], v) ^ 6 + (1 / 7) * bsxfun('min', u[i, ], v) ^ 7
 			k[i] = prod(z)
 		}
 	} else if (strcmp(ker, 'anovabspline')) {
-
 		for (i in 1:r1) {
 			z = 0
 			for (r in 0:(2*(kpar1+1))) {
@@ -1163,7 +1162,7 @@ CalcKernel <- function(u, v, ker, kpar1, kpar2) {
 	return(k)
 }
 
-test_smo <- function() {
+test_smo <- function(krnel = 'linear', C = 1, kpar1 = 0, kpar2 = 0) {
   
   # number of features
   l = 2
@@ -1175,7 +1174,7 @@ test_smo <- function() {
   set.seed(0)
   
   X1 = 10 * rand(N, l) - 5
-  y1 = array(y1, c(N, 1))
+  y1 = array(0, c(N, 1))
   
   for (i in 1:N) {
     
@@ -1188,10 +1187,6 @@ test_smo <- function() {
     }
   }
   
-  krnel = 'linear'
-  kpar1 = 0
-  kpar2 = 0
-  C = 2
   tol = 0.001
   steps = 100000
   eps = 10 ^ (-10)

f645354437f86713ee1f256933a57cce3cce27e5
Dael <dael.separa@gmail.com>
2016-04-10 11:10:23 +0800

    implement softmax activation on nnet_train. fix scaling bug.

--- NeuralNetwork/AdvancedNeuralNetwork.R
+++ NeuralNetwork/AdvancedNeuralNetwork.R
@@ -128,7 +128,7 @@ nnet_labels <- function(output, num_labels) {
   return(y_matrix)
 }
 
-nnet_train <- function(maxiter = 100, learning_rate = 0.1, tol = 10^(-3), training_set = array(0) , output = array(0), hidden_units = 0, num_labels = 1, min_max = 1, isGaussian = FALSE) {
+nnet_train <- function(maxiter = 100, learning_rate = 0.1, tol = 10^(-3), training_set = array(0) , output = array(0), hidden_units = 0, num_labels = 1, min_max = 1, isGaussian = FALSE, softmax = FALSE) {
 # Network training
   
   y_matrix = nnet_labels(output, num_labels)
@@ -146,18 +146,29 @@ nnet_train <- function(maxiter = 100, learning_rate = 0.1, tol = 10^(-3), traini
   
   y_k = numeric(0)
 	
+  m = nrow(training_set)
+  
   while (iter < maxiter && Error > tol) {
     # for training, perform forward and backpropagation each iteration, no regularization
-    forward = nnet_forward(training_set, w_ji, w_kj)
-    backward = nnet_backprop(training_set, forward$y_k, forward$z_2, forward$a_2, w_ji, w_kj, y_matrix, 0)
+    forward = nnet_forward(training_set, w_ji, w_kj, softmax)
+    backward = nnet_backprop(training_set, forward$y_k, forward$z_2, forward$a_2, w_ji, w_kj, y_matrix, 0, softmax)
     
     # update weights (using learning rate and gradient descent)
-    w_ji = w_ji - learning_rate * backward$dWji
-    w_kj = w_kj - learning_rate * backward$dWkj
-   
-  	
+    if (!softmax) {
+      w_ji = w_ji - learning_rate * backward$dWji
+      w_kj = w_kj - learning_rate * backward$dWkj
+    } else {
+      w_ji = w_ji - learning_rate * backward$dWji / m
+      w_kj = w_kj - learning_rate * backward$dWkj / m
+    }
+
     # save current performance
     Error = backward$Error
+    
+    if (softmax) {
+      Error = Error / m
+    }
+    
     y_k = forward$y_k
     
     iter = iter + 1
